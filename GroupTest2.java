d55c512cac73274c3d21243cfc2945c98fb6ad3c
ATIKUR RAHMAN <76957521+atikurrahman1996@users.noreply.github.com>
2023-12-30 12:49:16 +0600

    group test2

---
+++ GroupTest2.java
@@ -0,0 +1,59 @@
+package GroupingTest;
+
+import org.testng.annotations.Test;
+
+public class GroupTest2 {
+	
+		@Test(groups = { "sanity" })
+		void test1() {
+			System.out.println("This is Test1");
+		}
+
+		@Test(groups = { "sanity" })
+		void test2() {
+			System.out.println("This is Test2");
+		}
+
+		@Test(groups = { "regression" })
+		void test3() {
+			System.out.println("This is Test3");
+		}
+
+		@Test(groups = { "regression" })
+		void test4() {
+			System.out.println("This is Test4");
+		}
+		
+		
+		@Test(groups = { "smoke" })
+		void test5() {
+			System.out.println("This is Test5");
+		}
+		
+		@Test(groups = { "smoke" })
+		void test6() {
+			System.out.println("This is Test6");
+		}
+		
+		
+		@Test(groups = { "sanity", "smoke" })
+		void test7() {
+			System.out.println("This is Test7");
+		}
+		
+		
+
+		@Test(groups = { "sanity", "regression" })
+		void test8() {
+			System.out.println("This is Test8");
+		}
+		
+		
+
+		@Test(groups = {"functional"})
+		void test9() {
+			System.out.println("This is Test8");
+		}
+		
+
+}
